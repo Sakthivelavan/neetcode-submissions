48e4006e0fb6ccc29638f07cdcb9aae102fd8f60
Sakthivelavan <54427378+Sakthivelavan@users.noreply.github.com>
2026-07-26 01:46:45 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+    Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            // Opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+
+            // Closing brackets
+            else {
+
+                // No matching opening bracket
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        // If stack is empty, all brackets matched
+        return stack.isEmpty();
+    }
+
+    }
+
